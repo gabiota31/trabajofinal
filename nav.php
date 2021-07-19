5f8b64bed7b42989eb2108b6cfcdf7447de4fb0f
chayka <gabrielacpelaez@gmail.com>
2021-07-19 15:02:27 -0300

    intento de menu hambueguesa

--- nav.php
+++ nav.php
@@ -7,20 +7,34 @@
 
 </head>
 <body>
-    <div class="contenedor-nav">
-        <div>
-            <img src="img/logo.png" alt="logo organiza tus clases" class="logo">
+    <header>
+        <div class="contenedor-nav">
+            <div>
+                <img src="img/logo.png" alt="logo organiza tus clases" class="logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a class="b-nav" href="pag_principal.php">INICIO</a></li>
+                    <li><a class="b-nav" href="datos.php">TODOS LOS ESTUDIANTES</a></li>
+                    <li><a class="b-nav" href="nuevClase.php"> NUEVA CLASE </a></li>
+                    <li><a class="b-nav" href="datosClases.php"> TODAS LAS CLASES </a></li>
+                    
+                </ul>
+                
+                <div class="label">
+                    <input type="checkbox" name="" id="btn-menu">
+                    <label for="btn-menu"> <span class="usuario"> U </span> </label>
+                </div>
+                <div class="menu-usu">
+                    <ul>
+                        <li class="b-menu" id="b-top"><a class="item-menu" href="usuari.php" >PERFIL</a></li>
+                        <li class="b-menu"><a class="item-menu" href="cerrarSesion.php"> CERRAR SESION </a></li>
+                    </ul>
+                </div>
+
+            </nav>
         </div>
-        <ul>
-            <li><a class="b-nav" href="pag_principal.php">INICIO</a></li>
-            <li><a class="b-nav" href="datos.php">TODOS LOS ESTUDIANTES</a></li>
-            <li><a class="b-nav" href="nuevClase.php"> NUEVA CLASE </a></li>
-            <li><a class="b-nav" href="datosClases.php"> TODAS LAS CLASES </a></li>
-            <li><a class="b-nav" href="cerrarSesion.php"> CERRAR SESION </a></li>
-            <li> <a class="usuario" href="usuari.php" > <span> U </span> </a></li>
-        </ul>
-    </div>
-
-
+    </header>
+    <!-- -->
 </body>
 </html>
